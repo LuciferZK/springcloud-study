7527f939057c36a746b6a6c823b5610bc1c0b911
LuciferZK <13871278164@163.com>
2019-08-27 13:20:53 +0800

    将product服务配置纳入config-server管理

---
+++ morning-service-product/src/main/java/com/lucifer/product/config/MybatisPlusConfig.java
@@ -0,0 +1,30 @@
+package com.lucifer.product.config;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.PerformanceInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author: lucifer
+ * @date: 2019/8/23
+ * @description:
+ */
+@Configuration
+public class MybatisPlusConfig {
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
+    }
+
+    /**
+     * 性能分析拦截器，不建议生产使用   SQL执行效率插件
+     * 设置 dev test 环境开启
+     */
+    @Bean
+    // @Profile(value = {"dev", "local"})
+    public PerformanceInterceptor performanceInterceptor() {
+        System.out.println();
+        return new PerformanceInterceptor();
+    }
+}
